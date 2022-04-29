cbb83ced6aa6d882dd32b6220cac2431851d2373
EduSantosBrito <edu.santos.brito@gmail.com>
2022-04-29 02:12:41 -0300

    chore: add lint-staged and commitlint

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    test: { globals: true, environment: "jsdom" },
+    test: { globals: true, environment: "jsdom", setupFiles: "./test.setup.js" },
 });
